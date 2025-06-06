29672883095a95386f4edbdf5b999825b4cace64
stew-verse <gaming.stew.verse@gmail.com>
2025-06-06 21:15:28 +0530

    Update load_partial.js

--- js/load_partial.js
+++ js/load_partial.js
@@ -5,9 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   partials.forEach(el => {
     const url = el.getAttribute("data-include");
+
     fetch(url)
       .then(res => res.text())
-      .then(data => el.innerHTML = data)
+      .then(data => {
+        el.innerHTML = data;
+
+        // Dispatch custom event if navbar is loaded
+        if (url.includes("navbar.html")) {
+          document.dispatchEvent(new Event("navbar:ready"));
+        }
+      })
       .catch(err => console.error(`Error loading ${url}:`, err));
   });
 });
