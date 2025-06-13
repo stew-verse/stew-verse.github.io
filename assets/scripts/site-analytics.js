d019f13500e63407f8ba23e0ab2c92c11df5175c
stew-verse <gaming.stew.verse@gmail.com>
2025-06-13 11:08:23 +0530

    Create site-analytics.js

---
+++ assets/scripts/site-analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-5JSHJ036S2');
+gtag('config', 'G-5JSHJ036S2', {
+  'anonymize_ip': true // Set to true for IP anonymization
+});
